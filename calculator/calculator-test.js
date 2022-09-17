d32fd2b4c8dc3357297d94845f941f5480220796
Andres Sanchez <nevrsmer2@hotmail.com>
2022-09-17 17:15:18 -0400

    calculator/calculator-test.html

---
+++ calculator/calculator-test.js
@@ -0,0 +1,27 @@
+// describe("Testing if Correct Montly Payment is Returned", function () {
+//   it("should calculate montly payment of $132.15 based on values", function () {
+//     const values = {
+//       amount: 10000,
+//       years: 10,
+//       rate: 10,
+//     };
+
+//     expect(calculateMonthlyPayment(values)).toEqual("132.15");
+//   });
+// });
+
+// describe("Input Values", function () {
+//   it("should calculate current values", function () {
+//     const values = {
+//       amount: 10000,
+//       years: 10,
+//       rate: 10,
+//     };
+
+//     expect(getCurrentUIValues(values)).toEqual({
+//       amount: 10000,
+//       years: 10,
+//       rate: 10,
+//     });
+//   });
+// });
